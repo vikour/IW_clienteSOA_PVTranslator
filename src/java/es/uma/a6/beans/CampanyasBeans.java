6cb224be1012c72aacf2dc22df5e96b7c2886e88
Carlos <Carlos@alu-245-67.wifi.uma.es>
2018-11-06 19:39:45 +0100

    Se ha arreglado el webservice.

--- src/java/es/uma/a6/beans/CampanyasBeans.java
+++ src/java/es/uma/a6/beans/CampanyasBeans.java
@@ -5,8 +5,16 @@
  */
 package es.uma.a6.beans;
 
+
+import es.uma.a6.ws.Campaña;
+import es.uma.a6.ws.Modulo;
+import es.uma.a6.ws.WSPVTranslator_Service;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.xml.ws.WebServiceRef;
 
 /**
  *
@@ -16,10 +24,36 @@ import javax.enterprise.context.RequestScoped;
 @RequestScoped
 public class CampanyasBeans {
 
-    /**
-     * Creates a new instance of CampanyasBeans
-     */
+    @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_8080/WSPV_Translator/WSPV_Translator.wsdl")
+    private WSPVTranslator_Service service;
+
+    
+    
+    @EJB
+    private ConfigurationSessionBeans sesion;
+    
+    private Modulo modulo;
+    private List<Campaña> campañas;
+    
     public CampanyasBeans() {
     }
+    
+    @PostConstruct
+    public void init(){
+        modulo = sesion.getModulo();
+        campañas= this.findCampanyaByModulo(modulo);
+    }
+
+    private java.util.List<es.uma.a6.ws.Campaña> findCampanyaByModulo(es.uma.a6.ws.Modulo m) {
+        // Note that the injected javax.xml.ws.Service reference as well as port objects are not thread safe.
+        // If the calling of port operations may lead to race condition some synchronization is required.
+        es.uma.a6.ws.WSPVTranslator port = service.getWSPVTranslatorPort();
+        return port.findCampanyaByModulo(m);
+    }
+    
+    
+    
+    
+
     
 }
