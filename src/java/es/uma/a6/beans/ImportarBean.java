ffb1b1a2e745508c46128a10adf51136c1fb7cfa
Víctor Manuel Ortiz Guardeño <vikour92@gmail.com>
2018-11-12 21:19:24 +0100

    Falta que detecte el formato entre módulo y campaña.

---
+++ src/java/es/uma/a6/beans/ImportarBean.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.uma.a6.beans;
+
+import es.uma.a6.beans.importador.FormatoCampaña;
+import es.uma.a6.beans.importador.FormatoModulo;
+import es.uma.a6.beans.importador.Importador;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import javax.annotation.PostConstruct;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.Part;
+
+/**
+ *
+ * @author Vikour
+ */
+@Named(value = "importarBean")
+@RequestScoped
+public class ImportarBean {
+
+    private Part file;
+    
+    private static String VALID_CONTENT_TYPE = "application/x-zip-compressed";
+    private static String VALID_EXTENSION = ".zip";
+
+    @PostConstruct
+    public void init() {
+        file = null;
+    }
+    
+    public Part getFile() {
+        return file;
+    }
+
+    public void setFile(Part file) {
+        this.file = file;
+    }
+    
+    public void doImport() {
+        File f = null;
+        Importador importador;
+        
+        try {
+            if (file != null) {
+                f = subirFichero();
+                importador = new Importador(new FormatoCampaña(), f);
+                importador.importar();
+            }
+        }
+        catch (IOException ex) {
+            System.out.println("PERNEEE");
+        }
+        finally  {
+            if (f != null)
+                f.delete();
+        }
+        
+    }
+
+    private File subirFichero() {
+        File f = new File(file.getSubmittedFileName());
+        
+        try (InputStream input = file.getInputStream()) {
+            Files.copy(input, f.toPath());
+        }
+        catch(Exception ex) {
+            System.out.println("PENEE");
+            file = null;
+        }
+        
+        return f;
+    }
+
+    private boolean ficheroValido(Part file) {
+        boolean error = false;
+        
+        if (!file.getContentType().equals(VALID_CONTENT_TYPE) || 
+            !file.getSubmittedFileName().contains(VALID_EXTENSION)) 
+        {
+            FacesContext.getCurrentInstance()
+                    .addMessage(null, new FacesMessage("Se esperaba un fichero comprimido en ZIP."));
+            error = true;
+        }
+        
+        return !error;
+    }
+    
+    
+    
+}
---
+++ src/java/es/uma/a6/beans/importador/FormatoFichero.java
@@ -0,0 +1,18 @@
+
+package es.uma.a6.beans.importador;
+
+import java.io.File;
+
+public abstract class FormatoFichero {
+    
+    protected IFormatoFicheroNotificable notificar;
+    
+    public void setFormatoNotificable(IFormatoFicheroNotificable ctr) {
+        notificar = ctr;
+    }
+    
+    public abstract Object leer(File file);
+    public abstract void escribir(File file, Object object);
+    public abstract String getExtension();
+    
+}
